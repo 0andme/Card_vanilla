63ad59a0186b8a4f60b08d997954ce6ec11b18fc
0andme <yu00mi97@gmail.com>
2021-08-25 03:37:44 +0900

    feat : create js file
    js파일 추가 및 클릭시 comment의 span태그 값이 변경되는 예제 코드 추가

---
+++ card1/main.js
@@ -0,0 +1,10 @@
+let ex = document.querySelector('.card > .index > .comment');
+// console.log(ex);
+ex.addEventListener("click",hh);
+function hh()
+{
+  let a=self.document.querySelector("span.comment_cnt");
+  a.textContent="3"
+}
+console.log(ex);
+
